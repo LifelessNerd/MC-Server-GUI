04738dfaefb696e4ede69f64d84ab6789105c136
Luka <lukadevrij@gmail.com>
2022-11-11 23:27:45 +0100

    Console run mirror

---
+++ MC-Server-GUI/MC-Server-GUI/Form1.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MC_Server_GUI
+{
+    partial class RunEnvironmentForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RunEnvironmentForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rtbInput = new System.Windows.Forms.RichTextBox();
+            this.serverOutput = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.serverToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rtbInput);
+            this.panel1.Controls.Add(this.serverOutput);
+            this.panel1.Location = new System.Drawing.Point(339, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1211, 913);
+            this.panel1.TabIndex = 0;
+            // 
+            // rtbInput
+            // 
+            this.rtbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbInput.Location = new System.Drawing.Point(4, 875);
+            this.rtbInput.Multiline = false;
+            this.rtbInput.Name = "rtbInput";
+            this.rtbInput.Size = new System.Drawing.Size(1204, 35);
+            this.rtbInput.TabIndex = 1;
+            this.rtbInput.Text = "";
+            this.rtbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnterPressed);
+            // 
+            // serverOutput
+            // 
+            this.serverOutput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.serverOutput.Font = new System.Drawing.Font("Space Grotesk", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverOutput.ForeColor = System.Drawing.SystemColors.Info;
+            this.serverOutput.Location = new System.Drawing.Point(4, 4);
+            this.serverOutput.Name = "serverOutput";
+            this.serverOutput.ReadOnly = true;
+            this.serverOutput.Size = new System.Drawing.Size(1204, 864);
+            this.serverOutput.TabIndex = 0;
+            this.serverOutput.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(181, 26);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serverToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1904, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // serverToolStripMenuItem
+            // 
+            this.serverToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.runToolStripMenuItem,
+            this.stopToolStripMenuItem,
+            this.reloadToolStripMenuItem});
+            this.serverToolStripMenuItem.Name = "serverToolStripMenuItem";
+            this.serverToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.serverToolStripMenuItem.Text = "Server";
+            // 
+            // runToolStripMenuItem
+            // 
+            this.runToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("runToolStripMenuItem.Image")));
+            this.runToolStripMenuItem.Name = "runToolStripMenuItem";
+            this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.runToolStripMenuItem.Text = "Run";
+            this.runToolStripMenuItem.Click += new System.EventHandler(this.runToolStripMenuItem_Click);
+            // 
+            // stopToolStripMenuItem
+            // 
+            this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.stopToolStripMenuItem.Text = "Stop";
+            // 
+            // reloadToolStripMenuItem
+            // 
+            this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.reloadToolStripMenuItem.Text = "Reload";
+            // 
+            // RunEnvironmentForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1904, 978);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "RunEnvironmentForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem serverToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem runToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem stopToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox serverOutput;
+        private System.Windows.Forms.RichTextBox rtbInput;
+    }
+}
+
---
+++ MC-Server-GUI/MC-Server-GUI/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace MC_Server_GUI
+{
+    public partial class RunEnvironmentForm : Form
+    {
+        Process cmdProcess = null;
+        StreamWriter stdin = null;
+        public RunEnvironmentForm()
+        {
+            InitializeComponent();
+            serverOutput.ReadOnly = true;
+
+        }
+
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StartCmdProcess();
+        }
+
+        private void StartCmdProcess()
+        {
+            var pStartInfo = new ProcessStartInfo
+            {
+                FileName = "cmd.exe",
+                // Batch File Arguments = "/C START /b /WAIT somebatch.bat",
+                // Test: Arguments = "START /WAIT /K ipconfig /all",
+                Arguments = "START /WAIT",
+                WorkingDirectory = Environment.SystemDirectory,
+                // WorkingDirectory = Application.StartupPath,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+                WindowStyle = ProcessWindowStyle.Hidden,
+            };
+
+            cmdProcess = new Process
+            {
+                StartInfo = pStartInfo,
+                EnableRaisingEvents = true,
+                // Test without and with this
+                // When SynchronizingObject is set, no need to BeginInvoke()
+                //SynchronizingObject = this
+            };
+
+            cmdProcess.Start();
+            cmdProcess.BeginErrorReadLine();
+            cmdProcess.BeginOutputReadLine();
+            stdin = cmdProcess.StandardInput;
+            // stdin.AutoFlush = true;  <- already true
+            stdin.Write("cd C:\\Users\\Menee\\Desktop\\JavaSample\\out\\artifacts\\JavaSample_jar" + Environment.NewLine);
+            stdin.Write("java -jar JavaSample.jar" + Environment.NewLine);
+
+            rtbInput.Multiline = true;
+            
+
+            cmdProcess.OutputDataReceived += (s, evt) => {
+                if (evt.Data != null)
+                {
+                    
+                    BeginInvoke(new MethodInvoker(() => {
+
+                        if (!(evt.Data.Contains(">"))) //TODO: might cause problems later
+                        {
+                            serverOutput.AppendText(evt.Data + Environment.NewLine);
+                            serverOutput.ScrollToCaret();
+                        }
+                    }));
+                }
+            };
+
+            cmdProcess.ErrorDataReceived += (s, evt) => {
+                if (evt.Data != null)
+                {
+                    BeginInvoke(new Action(() => {
+                        serverOutput.SelectionColor = Color.Red;
+                        serverOutput.AppendText(evt.Data + Environment.NewLine);
+                        serverOutput.ScrollToCaret();
+                        
+                    }));
+                }
+            };
+
+            cmdProcess.Exited += (s, evt) => {
+                stdin?.Dispose();
+                cmdProcess?.Dispose();
+            };
+
+
+        }
+
+        private void EnterPressed(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                if (stdin == null)
+                {
+                    serverOutput.AppendText("Process not started" + Environment.NewLine);
+                    return;
+                }
+
+                e.Handled = true;
+                if (stdin.BaseStream.CanWrite)
+                {
+                    stdin.Write(rtbInput.Text + Environment.NewLine);
+                    serverOutput.AppendText("> " + rtbInput.Text + Environment.NewLine);
+                    // To write to a Console app, just 
+                    // stdin.WriteLine(rtbStdIn.Text); 
+                }
+                rtbInput.Clear();
+            }
+        }
+    }
+}
