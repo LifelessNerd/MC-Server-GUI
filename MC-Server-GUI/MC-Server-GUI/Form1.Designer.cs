f52f1ebc8c126be1609ac73ac2e8ea23f01088be
Luka <lukadevrij@gmail.com>
2022-11-12 20:44:49 +0100

    BUTTONS, player list, command history

--- MC-Server-GUI/MC-Server-GUI/Form1.Designer.cs
+++ MC-Server-GUI/MC-Server-GUI/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RunEnvironmentForm));
             this.panel1 = new System.Windows.Forms.Panel();
             this.rtbInput = new System.Windows.Forms.RichTextBox();
             this.serverOutput = new System.Windows.Forms.RichTextBox();
@@ -40,40 +39,58 @@
             this.runToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.stopToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.reloadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.onlinePlayersListBox = new System.Windows.Forms.ListBox();
+            this.kickButton = new System.Windows.Forms.Button();
+            this.toggleOPButton = new System.Windows.Forms.Button();
+            this.banButton = new System.Windows.Forms.Button();
+            this.rtbIP = new System.Windows.Forms.RichTextBox();
+            this.reloadPic = new System.Windows.Forms.PictureBox();
+            this.stopPic = new System.Windows.Forms.PictureBox();
+            this.runPic = new System.Windows.Forms.PictureBox();
+            this.showHidePic = new System.Windows.Forms.PictureBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel1.SuspendLayout();
             this.contextMenuStrip1.SuspendLayout();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stopPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.runPic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showHidePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.Controls.Add(this.rtbInput);
             this.panel1.Controls.Add(this.serverOutput);
-            this.panel1.Location = new System.Drawing.Point(339, 53);
+            this.panel1.Location = new System.Drawing.Point(339, 27);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1211, 913);
+            this.panel1.Size = new System.Drawing.Size(1211, 972);
             this.panel1.TabIndex = 0;
             // 
             // rtbInput
             // 
+            this.rtbInput.BackColor = System.Drawing.SystemColors.WindowFrame;
             this.rtbInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbInput.Location = new System.Drawing.Point(4, 875);
+            this.rtbInput.Location = new System.Drawing.Point(3, 934);
             this.rtbInput.Multiline = false;
             this.rtbInput.Name = "rtbInput";
             this.rtbInput.Size = new System.Drawing.Size(1204, 35);
             this.rtbInput.TabIndex = 1;
             this.rtbInput.Text = "";
+            this.rtbInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.CheckUpDown);
             this.rtbInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.EnterPressed);
             // 
             // serverOutput
             // 
-            this.serverOutput.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.serverOutput.Font = new System.Drawing.Font("Space Grotesk", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.serverOutput.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(58)))), ((int)(((byte)(64)))));
+            this.serverOutput.Font = new System.Drawing.Font("Lucida Sans Unicode", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.serverOutput.ForeColor = System.Drawing.SystemColors.Info;
             this.serverOutput.Location = new System.Drawing.Point(4, 4);
             this.serverOutput.Name = "serverOutput";
             this.serverOutput.ReadOnly = true;
-            this.serverOutput.Size = new System.Drawing.Size(1204, 864);
+            this.serverOutput.Size = new System.Drawing.Size(1204, 924);
             this.serverOutput.TabIndex = 0;
             this.serverOutput.Text = "";
             // 
@@ -113,7 +130,7 @@
             // 
             // runToolStripMenuItem
             // 
-            this.runToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("runToolStripMenuItem.Image")));
+            this.runToolStripMenuItem.Image = global::MC_Server_GUI.Properties.Resources.run;
             this.runToolStripMenuItem.Name = "runToolStripMenuItem";
             this.runToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.runToolStripMenuItem.Text = "Run";
@@ -121,24 +138,154 @@
             // 
             // stopToolStripMenuItem
             // 
+            this.stopToolStripMenuItem.Image = global::MC_Server_GUI.Properties.Resources.Untitled;
             this.stopToolStripMenuItem.Name = "stopToolStripMenuItem";
-            this.stopToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.stopToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.stopToolStripMenuItem.Text = "Stop";
             // 
             // reloadToolStripMenuItem
             // 
+            this.reloadToolStripMenuItem.Image = global::MC_Server_GUI.Properties.Resources.reload;
             this.reloadToolStripMenuItem.Name = "reloadToolStripMenuItem";
-            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(110, 22);
+            this.reloadToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.reloadToolStripMenuItem.Text = "Reload";
+            // 
+            // onlinePlayersListBox
+            // 
+            this.onlinePlayersListBox.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.onlinePlayersListBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onlinePlayersListBox.FormattingEnabled = true;
+            this.onlinePlayersListBox.ItemHeight = 25;
+            this.onlinePlayersListBox.Items.AddRange(new object[] {
+            "Lorem",
+            "ipsum",
+            "dolor",
+            "sit",
+            "amet"});
+            this.onlinePlayersListBox.Location = new System.Drawing.Point(1567, 31);
+            this.onlinePlayersListBox.Name = "onlinePlayersListBox";
+            this.onlinePlayersListBox.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.onlinePlayersListBox.Size = new System.Drawing.Size(325, 154);
+            this.onlinePlayersListBox.TabIndex = 3;
+            // 
+            // kickButton
+            // 
+            this.kickButton.Font = new System.Drawing.Font("Space Grotesk", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.kickButton.Location = new System.Drawing.Point(1567, 191);
+            this.kickButton.Name = "kickButton";
+            this.kickButton.Size = new System.Drawing.Size(103, 39);
+            this.kickButton.TabIndex = 4;
+            this.kickButton.Text = "KICK";
+            this.kickButton.UseVisualStyleBackColor = true;
+            // 
+            // toggleOPButton
+            // 
+            this.toggleOPButton.Font = new System.Drawing.Font("Space Grotesk", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toggleOPButton.Location = new System.Drawing.Point(1679, 191);
+            this.toggleOPButton.Name = "toggleOPButton";
+            this.toggleOPButton.Size = new System.Drawing.Size(103, 39);
+            this.toggleOPButton.TabIndex = 5;
+            this.toggleOPButton.Text = "TOGGLE OP";
+            this.toggleOPButton.UseVisualStyleBackColor = true;
+            // 
+            // banButton
+            // 
+            this.banButton.Font = new System.Drawing.Font("Space Grotesk", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.banButton.Location = new System.Drawing.Point(1789, 191);
+            this.banButton.Name = "banButton";
+            this.banButton.Size = new System.Drawing.Size(103, 39);
+            this.banButton.TabIndex = 6;
+            this.banButton.Text = "BAN";
+            this.banButton.UseVisualStyleBackColor = true;
+            // 
+            // rtbIP
+            // 
+            this.rtbIP.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rtbIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbIP.Location = new System.Drawing.Point(12, 37);
+            this.rtbIP.Multiline = false;
+            this.rtbIP.Name = "rtbIP";
+            this.rtbIP.ReadOnly = true;
+            this.rtbIP.Size = new System.Drawing.Size(279, 34);
+            this.rtbIP.TabIndex = 7;
+            this.rtbIP.Text = "";
+            this.rtbIP.Visible = false;
+            // 
+            // reloadPic
+            // 
+            this.reloadPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.reloadPic.Image = global::MC_Server_GUI.Properties.Resources.reload;
+            this.reloadPic.Location = new System.Drawing.Point(230, 78);
+            this.reloadPic.Name = "reloadPic";
+            this.reloadPic.Size = new System.Drawing.Size(96, 77);
+            this.reloadPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.reloadPic.TabIndex = 10;
+            this.reloadPic.TabStop = false;
+            this.reloadPic.Click += new System.EventHandler(this.reloadPic_Click);
+            // 
+            // stopPic
+            // 
+            this.stopPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.stopPic.Image = global::MC_Server_GUI.Properties.Resources.Untitled;
+            this.stopPic.Location = new System.Drawing.Point(121, 78);
+            this.stopPic.Name = "stopPic";
+            this.stopPic.Size = new System.Drawing.Size(96, 77);
+            this.stopPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.stopPic.TabIndex = 9;
+            this.stopPic.TabStop = false;
+            this.stopPic.Click += new System.EventHandler(this.stopPic_Click);
+            // 
+            // runPic
+            // 
+            this.runPic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.runPic.Image = global::MC_Server_GUI.Properties.Resources.run;
+            this.runPic.Location = new System.Drawing.Point(12, 78);
+            this.runPic.Name = "runPic";
+            this.runPic.Size = new System.Drawing.Size(96, 77);
+            this.runPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.runPic.TabIndex = 8;
+            this.runPic.TabStop = false;
+            this.runPic.Click += new System.EventHandler(this.runPic_Click);
+            // 
+            // showHidePic
+            // 
+            this.showHidePic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.showHidePic.Image = global::MC_Server_GUI.Properties.Resources.showhide;
+            this.showHidePic.Location = new System.Drawing.Point(297, 37);
+            this.showHidePic.Name = "showHidePic";
+            this.showHidePic.Size = new System.Drawing.Size(36, 34);
+            this.showHidePic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.showHidePic.TabIndex = 11;
+            this.showHidePic.TabStop = false;
+            this.toolTip1.SetToolTip(this.showHidePic, "Show/hide IP adress");
+            this.showHidePic.Click += new System.EventHandler(this.showHidePic_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(14, 39);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(275, 30);
+            this.pictureBox1.TabIndex = 12;
+            this.pictureBox1.TabStop = false;
             // 
             // RunEnvironmentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.Window;
-            this.ClientSize = new System.Drawing.Size(1904, 978);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(48)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(1904, 1011);
+            this.Controls.Add(this.showHidePic);
+            this.Controls.Add(this.reloadPic);
+            this.Controls.Add(this.stopPic);
+            this.Controls.Add(this.runPic);
+            this.Controls.Add(this.rtbIP);
+            this.Controls.Add(this.banButton);
+            this.Controls.Add(this.toggleOPButton);
+            this.Controls.Add(this.kickButton);
+            this.Controls.Add(this.onlinePlayersListBox);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "RunEnvironmentForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -148,6 +295,11 @@
             this.contextMenuStrip1.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.reloadPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.stopPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.runPic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.showHidePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -165,6 +317,17 @@
         private System.Windows.Forms.ToolStripMenuItem reloadToolStripMenuItem;
         private System.Windows.Forms.RichTextBox serverOutput;
         private System.Windows.Forms.RichTextBox rtbInput;
+        private System.Windows.Forms.ListBox onlinePlayersListBox;
+        private System.Windows.Forms.Button kickButton;
+        private System.Windows.Forms.Button toggleOPButton;
+        private System.Windows.Forms.Button banButton;
+        private System.Windows.Forms.RichTextBox rtbIP;
+        private System.Windows.Forms.PictureBox runPic;
+        private System.Windows.Forms.PictureBox stopPic;
+        private System.Windows.Forms.PictureBox reloadPic;
+        private System.Windows.Forms.PictureBox showHidePic;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- MC-Server-GUI/MC-Server-GUI/Form1.cs
+++ MC-Server-GUI/MC-Server-GUI/Form1.cs
@@ -4,8 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.Drawing.Text;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,11 +18,24 @@ namespace MC_Server_GUI
     public partial class RunEnvironmentForm : Form
     {
         Process cmdProcess = null;
-        StreamWriter stdin = null;
+        StreamWriter input = null;
+
+        String ip;
+        bool ipHidden = true;
+
+        List<String> onlinePlayers = new List<String>();
+
+        List<String> inputHistory = new List<String>();
+        int historyIndex = 0;
+
         public RunEnvironmentForm()
         {
             InitializeComponent();
             serverOutput.ReadOnly = true;
+
+            inputHistory.Add("dab");
+            inputHistory.Add("dab2");
+            int historyIndex = inputHistory.Count;
 
         }
 
@@ -28,9 +43,27 @@ namespace MC_Server_GUI
         {
             StartCmdProcess();
         }
+        private void stopToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            stopServer();
+        }
+        private void reloadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (input == null)
+            {
+                serverOutput.AppendText("Process not started" + Environment.NewLine);
+                return;
+            }
+            else
+            {
+                input.WriteLine("reload confirm" + Environment.NewLine);
+            }
+        }
 
         private void StartCmdProcess()
         {
+            serverOutput.SelectionColor = Color.Green;
+            serverOutput.AppendText("STARTING SERVER..." + Environment.NewLine);
             var pStartInfo = new ProcessStartInfo
             {
                 FileName = "cmd.exe",
@@ -47,80 +80,202 @@ namespace MC_Server_GUI
                 WindowStyle = ProcessWindowStyle.Hidden,
             };
 
+            //Update IP field
+            try
+            {
+                string thingy = new WebClient().DownloadString("http://checkip.dyndns.org");
+                string[] ipSplit = thingy.Split(' ');
+                ip = ipSplit[ipSplit.Length - 1];
+                ipSplit = ip.Split('<');
+                ip = ipSplit[0];
+                rtbIP.Text = ip + ":25565"; //TODO: change based on properties.txt
+            } catch (Exception e)
+            {
+                rtbIP.Text = e.Message;
+            }
+
             cmdProcess = new Process
             {
                 StartInfo = pStartInfo,
                 EnableRaisingEvents = true,
-                // Test without and with this
-                // When SynchronizingObject is set, no need to BeginInvoke()
-                //SynchronizingObject = this
+
             };
 
             cmdProcess.Start();
             cmdProcess.BeginErrorReadLine();
             cmdProcess.BeginOutputReadLine();
-            stdin = cmdProcess.StandardInput;
+            input = cmdProcess.StandardInput;
             // stdin.AutoFlush = true;  <- already true
-            stdin.Write("cd C:\\Users\\Menee\\Desktop\\JavaSample\\out\\artifacts\\JavaSample_jar" + Environment.NewLine);
-            stdin.Write("java -jar JavaSample.jar" + Environment.NewLine);
+            input.Write("cd C:\\Users\\Menee\\Desktop\\Minecraft Server Plugin Tester 1.19" + Environment.NewLine);
+            //stdin.Write("java -Xmx2048M -Xms2048M -jar paper-1.19.2-263.jar -nogui" + Environment.NewLine);
+            input.Write("java -jar JavaSample.jar" + Environment.NewLine);
 
             rtbInput.Multiline = true;
-            
 
-            cmdProcess.OutputDataReceived += (s, evt) => {
-                if (evt.Data != null)
-                {
-                    
-                    BeginInvoke(new MethodInvoker(() => {
-
-                        if (!(evt.Data.Contains(">"))) //TODO: might cause problems later
-                        {
-                            serverOutput.AppendText(evt.Data + Environment.NewLine);
-                            serverOutput.ScrollToCaret();
-                        }
-                    }));
-                }
-            };
-
-            cmdProcess.ErrorDataReceived += (s, evt) => {
-                if (evt.Data != null)
-                {
-                    BeginInvoke(new Action(() => {
-                        serverOutput.SelectionColor = Color.Red;
-                        serverOutput.AppendText(evt.Data + Environment.NewLine);
-                        serverOutput.ScrollToCaret();
-                        
-                    }));
-                }
-            };
+            cmdProcess.OutputDataReceived += this.InputReceived;
 
             cmdProcess.Exited += (s, evt) => {
-                stdin?.Dispose();
+                input?.Dispose();
                 cmdProcess?.Dispose();
             };
 
+        }
+
+        private void InputReceived(object o, DataReceivedEventArgs evt){
+            if (evt.Data != null)
+            {
+
+                BeginInvoke(new MethodInvoker(() => {
+
+                    if (!(evt.Data.Contains(">"))) //TODO: might cause problems later
+                    {
+                        serverOutput.AppendText(evt.Data + Environment.NewLine);
+                        serverOutput.ScrollToCaret();
+                    }
+
+                    if (evt.Data.Contains("joined the game"))
+                    {
+                        String[] dataSplit = evt.Data.Split(' ');
+                        onlinePlayers.Add(dataSplit[dataSplit.Length - 4]);
+                        updatePlayerList();
+                    }
+                    else if (evt.Data.Contains("left the game"))
+                    {
+                        String[] dataSplit = evt.Data.Split(' ');
+                        onlinePlayers.Remove(dataSplit[dataSplit.Length - 4]);
+                        updatePlayerList();
+                    }
+
+                }));
+            }
+        }
+
+        private void updatePlayerList()
+        {
+            onlinePlayersListBox.Items.Clear();
+            foreach (String player in onlinePlayers)
+            {
+                onlinePlayersListBox.Items.Add(player);
+            }
 
         }
 
+
         private void EnterPressed(object sender, KeyPressEventArgs e)
         {
+
             if (e.KeyChar == (char)Keys.Enter)
             {
-                if (stdin == null)
+                if (input == null)
                 {
                     serverOutput.AppendText("Process not started" + Environment.NewLine);
                     return;
                 }
-
+                historyIndex = inputHistory.Count;
                 e.Handled = true;
-                if (stdin.BaseStream.CanWrite)
+                if (input.BaseStream.CanWrite)
                 {
-                    stdin.Write(rtbInput.Text + Environment.NewLine);
-                    serverOutput.AppendText("> " + rtbInput.Text + Environment.NewLine);
+                    string rtbInputText = rtbInput.Text;
+                    rtbInputText = rtbInputText.Replace("\n", "");
+                    input.Write(rtbInputText + Environment.NewLine);
+                    inputHistory.Add(rtbInputText);
+                    historyIndex = inputHistory.Count;
+                    serverOutput.AppendText("> " + rtbInputText + Environment.NewLine);
                     // To write to a Console app, just 
                     // stdin.WriteLine(rtbStdIn.Text); 
+                    checkCustomKeywords();
+                    
                 }
                 rtbInput.Clear();
+            }
+            
+
+        }
+        private void CheckUpDown(object sender, KeyEventArgs e)
+        {
+           
+            if (e.KeyCode == Keys.Up)
+            {
+                if (historyIndex - 1 >= 0) {
+                    rtbInput.Multiline = false;
+                    if (historyIndex == inputHistory.Count)
+                    {
+                        inputHistory.Add(rtbInput.Text);
+
+                    }
+                    historyIndex--;
+                    rtbInput.Text = inputHistory[historyIndex];
+
+                    rtbInput.Multiline = true;
+                }
+            }
+            if (e.KeyCode == Keys.Down)
+            {
+                if (historyIndex + 1 < inputHistory.Count) {
+                    rtbInput.Multiline = false;
+                    historyIndex++;
+                    rtbInput.Text = inputHistory[historyIndex];
+
+                    rtbInput.Multiline = true;
+                }
+            }
+            
+        }
+
+        private void checkCustomKeywords()
+        {
+            Console.WriteLine(rtbInput.Text);
+            if (rtbInput.Text.Contains("stop"))
+            {
+                stopServer();
+            }
+            
+        }
+        private void stopServer()
+        {
+            input = null;
+            rtbInput.Multiline = false;
+            onlinePlayers.Clear();
+            updatePlayerList();
+            serverOutput.SelectionColor = Color.Red;
+            serverOutput.AppendText("SERVER STOPPING" + Environment.NewLine);
+        }
+
+
+        private void runPic_Click(object sender, EventArgs e)
+        {
+            StartCmdProcess();
+        }
+
+        private void stopPic_Click(object sender, EventArgs e)
+        {
+            stopServer();
+        }
+
+        private void reloadPic_Click(object sender, EventArgs e)
+        {
+            
+            if (input == null)
+            {
+                serverOutput.AppendText("Process not started" + Environment.NewLine);
+                return;
+            }
+            else
+            {
+                input.WriteLine("reload confirm" + Environment.NewLine);
+            }
+        }
+
+        private void showHidePic_Click(object sender, EventArgs e)
+        {
+            if (ipHidden)
+            {
+                ipHidden = false;
+                rtbIP.Show();
+            } else
+            {
+                rtbIP.Hide();
+                ipHidden = true;
             }
         }
     }
